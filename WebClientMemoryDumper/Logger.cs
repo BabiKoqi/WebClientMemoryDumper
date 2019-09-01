8a49b362a0d755d1aa4bb9f2a4779f28acd26258
SilentHammerHUN <zsirayricsi@gmail.com>
2019-09-01 11:29:01 +0200

    Add skeleton.

---
+++ WebClientMemoryDumper/Logger.cs
@@ -0,0 +1,5 @@
+﻿namespace WebClientMemoryDumper {
+    static class Logger {
+        
+    }
+}
--- WebClientMemoryDumper/Program.cs
+++ WebClientMemoryDumper/Program.cs
@@ -1,5 +1,12 @@
-﻿namespace WebClientMemoryDumper {
-    internal class Program {
-        public static void Main(string[] args) { }
+﻿using Colorful;
+
+namespace WebClientMemoryDumper {
+    static class Program {
+        static void Main(string[] args) {
+            Console.WriteAscii("WebClient MemDumper v1.0.0");
+            Console.WriteLine("by xsilent007");
+            
+            
+        }
     }
 }
