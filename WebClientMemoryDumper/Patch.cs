a766392e29d95b7999ddc147182121df719c1ab5
SilentHammerHUN <zsirayricsi@gmail.com>
2019-09-04 15:40:54 +0200

    Fix xNet support

---
+++ WebClientMemoryDumper/Patch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Harmony;
+using Console = Colorful.Console;
+
+namespace WebClientMemoryDumper {
+    [HarmonyPatch(typeof(Uri))]
+    [HarmonyPatch("CreateThis")]
+    [HarmonyPatch(new [] { typeof(string), typeof(bool), typeof(UriKind) })]
+    class Patch {
+        static readonly HashSet<string> _urls = new HashSet<string>();
+        
+        static void Postfix(string uri, bool dontEscape, UriKind uriKind) {
+            if (_urls.Contains(uri))
+                return;
+
+            _urls.Add(uri);
+            
+            Console.Write("[", Color.White);
+            Console.Write("+", Color.Lime);
+            Console.Write("] ", Color.White);
+            Console.WriteLine(uri, Color.DarkGray);
+        }
+    }
+}
